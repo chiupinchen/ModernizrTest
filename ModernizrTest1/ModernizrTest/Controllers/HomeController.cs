d11028e37460cbe3fc5663932a171af3a7e71952
unknown <cchen@CI1220.children.org>
2012-08-03 10:15:50 -0500

    added

---
+++ ModernizrTest1/ModernizrTest/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StackExchange.Profiling;
+using System.Threading;
+
+namespace ModernizrTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var profiler = MiniProfiler.Current; // it's ok if this is null
+            using (profiler.Step("Init"))
+            {
+                ViewBag.Message = "Welcome to ASP.NET MVC!";
+            }
+            using (profiler.Step("A"))
+            {
+                using (profiler.Step("Step A"))
+                { // something more interesting here
+                    Thread.Sleep(100);
+                }
+                using (profiler.Step("Step B"))
+                { // and here
+                    Thread.Sleep(250);
+                }
+            }
+            
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ModernizrTest1/ModernizrTest/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using StackExchange.Profiling;
+
+namespace ModernizrTest
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
+        }
+
+        protected void Application_BeginRequest()
+        {
+
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
